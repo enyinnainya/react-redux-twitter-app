46d495fe5e94a1d0595b6980366434af1f354256
Enyinna Inya <enyinnai@phreetech.com>
2020-04-06 21:02:12 +0100

    working on twitter-like app with react redux

---
+++ src/components/Dashboard.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import Tweet from './Tweet';
+
+class Dashboard extends Component {
+
+  render() {
+    const { tweetsIds } = this.props;
+    return (
+      <div>
+        <h3 className="center">Your Timeline</h3>
+        <ul className="dashboard-list">
+          {
+            tweetsIds.map((tweetId) => {
+              return (
+                <li key={tweetId}>
+                  <Tweet id={tweetId} />
+                </li>
+              );
+            })
+          }
+        </ul>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = ({ tweets }) => {
+  return {
+    tweetsIds: Object.keys(tweets)
+      .sort((a,b)=>tweets[b].timestamp - tweets[a].timestamp)
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard);
---
+++ src/components/NewTweet.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { handleAddTweet } from '../actions/tweets';
+import { Redirect } from 'react-router-dom';
+
+class NewTweet extends Component {
+
+  state = {
+    text: '',
+    toHome: false
+  }
+  handleChange = (e) => {
+    const text = e.target.value;
+    this.setState(() => ({
+      text
+    }))
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { text } = this.state;
+    const { dispatch, id } = this.props;
+    console.log("New Tweet"); console.log(this.props)
+    dispatch(handleAddTweet(text, id));
+    this.setState(() => ({
+      text: '',
+      toHome: id ? false : true
+    }))
+
+  }
+  render() {
+
+    const { text, toHome } = this.state;
+    const tweetLeft = 280 - text.length;
+    if (toHome === true) {
+      return <Redirect to="/" />
+    }
+    {/* redirect to home view if submitted  */}
+    return (
+      <div>
+        <h3 className="center">Compose new Tweet</h3>
+        <form className="new-tweet" onSubmit={this.handleSubmit}>
+          <textarea 
+            placeholder="What's happening?"
+            value={text}
+            onChange={this.handleChange}
+            className="textarea"
+            maxLength={280}></textarea>
+          {tweetLeft <= 100 && (
+            <div className="tweet-length">{tweetLeft}</div>
+          )}
+          <button
+            className="btn"
+            type="submit"
+            disabled={text===""?true:false}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    )
+  }
+}
+
+// const mapStateToProps = ({ authedUser }) => {
+//   return {
+//     loading: authedUser === null
+//   }
+// }
+export default connect()(NewTweet)
